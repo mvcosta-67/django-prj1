6ee54fc9c62eddad1b2187f09583ac2e5dbc95ff
mvcosta-67 <marcos.v.costa@uol.com.br>
2025-08-18 19:48:51 -0300

    removido contato e sobre

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps
+    'recipes',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,11 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates', 
+            BASE_DIR / 'recipes' / 'templates_temp', 
+            
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
